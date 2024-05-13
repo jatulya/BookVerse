9ed8e4f6e87aadfa67fdd19849184d0b29006ab7
J-Atulya <atulyajayakrishnan@gmail.com>
2024-05-13 13:24:44 +0530

    set up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>     
+    </Router>
   );
 }
 
